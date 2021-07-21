1f7b25553df657388398c904d85d7c6fe4599821
fennecJ <hwahwa649@gmail.com>
2021-07-21 12:58:25 +0800

    Prevent from undefined behavior in rot.c

--- enroll_test/rot.c
+++ enroll_test/rot.c
@@ -1,18 +1,19 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+
 #define __DECLARE_ROTATE(bits, type)                                           \
     static inline type rotl##bits(const type v, int c) {                       \
         const int mask = (bits) - (1);                                         \
         c &= mask;                                                             \
                                                                                \
-        return ((v << c) | (v >> (bits - c)));                                 \
+        return (v << c) | (v >> (-c & mask));                                  \
     }                                                                          \
     static inline type rotr##bits(const type v, int c) {                       \
         const int mask = (bits) - (1);                                         \
         c &= mask;                                                             \
                                                                                \
-        return ((v >> c) | (v << (bits - c)));                                 \
+        return ((v >> c) | (v << (-c & mask));                                 \
     }
 #define DECLARE_ROTATE(bits) __DECLARE_ROTATE(bits, uint##bits##_t)
 
